337ef392144f1a8f4562ac0f715b542dbf8ea3f7
Pansir1025 <pansir1027@163.com>
2022-07-13 21:46:54 +0800

    Hello3

--- src/main/java/com/acwer/GitTest.java
+++ src/main/java/com/acwer/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello1");
         System.out.println("hello2");
+        System.out.println("hello3");
+        System.out.println("hello4");
     }
 }
